083dfb6f9f979c44bf69b81ff5761b452eb26c8d
Seong, Hyunwoo <ssssww1135@gmail.com>
2022-09-26 17:06:18 +0900

    Main.java 수정 #4

--- src/e202014034/Main.java
+++ src/e202014034/Main.java
@@ -1,12 +1,18 @@
 package e202014034;
 
 public class Main {
+
+    static void say() {
+        System.out.println("main");
+    }
     
     public static void main(String[] args) {
+        say();
         Hello hello = new Hello();
         hello.say("hello world");
     }
 }
+
 
 
 
