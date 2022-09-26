b5b6c26c46eaf1a2d2268a0e3e4a0549c56ff3f7
Seong, Hyunwoo <ssssww1135@gmail.com>
2022-09-26 16:57:42 +0900

    Main.java 수정 #2, Hello.java 수정 #2

--- src/e202014034/Hello.java
+++ src/e202014034/Hello.java
@@ -2,7 +2,8 @@ package e202014034;
 
 public class Hello {
 
-    public void say() {
-        System.out.println("hello");
+    public void say(String msg) {
+        System.out.println(msg);
     }
 }
+
--- src/e202014034/Main.java
+++ src/e202014034/Main.java
@@ -4,8 +4,9 @@ public class Main {
     
     public static void main(String[] args) {
         Hello hello = new Hello();
-        hello.say();
+        hello.say("hello world");
     }
 }
+
 
 
